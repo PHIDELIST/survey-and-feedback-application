1a8694c4d5652c0aa2b7576aaf0258bf9508697e
PHIDELIST <phidelisoluoch@gmail.com>
2023-06-13 01:02:53 +0300

    Added pop up feedback form

---
+++ frontend-react-js/src/Forms/FeedBackForm.jsx
@@ -0,0 +1,88 @@
+import React, { useState } from 'react';
+import Modal from 'react-modal';
+
+const modalStyles = {
+  overlay: {
+    backgroundColor: 'rgba(0, 0, 0, 0.5)',
+    zIndex: 9999,
+  },
+  content: {
+    width: '400px',
+    margin: '0 auto',
+    padding: '20px',
+    border: '1px solid #ccc',
+    borderRadius: '4px',
+    background: '#fff',
+  },
+};
+
+function FeedbackForm() {
+  const [feedbackType, setFeedbackType] = useState('');
+  const [feedbackText, setFeedbackText] = useState('');
+  const [isModalOpen, setIsModalOpen] = useState(false);
+
+  const handleFormSubmit = (event) => {
+    event.preventDefault();
+
+    // Perform validation or additional logic if needed
+
+    // Submit feedback to the backend or perform any desired actions
+    console.log('Feedback submitted:', feedbackType, feedbackText);
+
+    // Reset the form
+    setFeedbackType('');
+    setFeedbackText('');
+
+    // Close the modal
+    setIsModalOpen(false);
+  };
+
+  const openModal = (type) => {
+    setFeedbackType(type);
+    setIsModalOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsModalOpen(false);
+  };
+
+  return (
+    <div>
+      <h2>Feedback Form</h2>
+      <div className="feedback-options">
+        <button onClick={() => openModal('thumbs-up')}>
+          <span role="img" aria-label="Thumbs Up">👍</span>
+        </button>
+        <button onClick={() => openModal('thumbs-down')}>
+          <span role="img" aria-label="Thumbs Down">👎</span>
+        </button>
+      </div>
+
+      <Modal
+        isOpen={isModalOpen}
+        onRequestClose={closeModal}
+        contentLabel="Feedback Form"
+        style={modalStyles}
+      >
+        <h2>Feedback Form</h2>
+        <form onSubmit={handleFormSubmit}>
+          <p>Selected Feedback Type: {feedbackType}</p>
+
+          <label htmlFor="feedbackText">Additional Comments:</label>
+          <textarea
+            id="feedbackText"
+            value={feedbackText}
+            onChange={(e) => setFeedbackText(e.target.value)}
+            rows={4}
+          />
+
+          <button type="submit">
+            Submit Feedback
+          </button>
+        </form>
+      </Modal>
+    </div>
+  );
+}
+
+export default FeedbackForm;
--- frontend-react-js/src/pages/AboutPage.jsx
+++ frontend-react-js/src/pages/AboutPage.jsx
@@ -1,7 +1,9 @@
 import React from 'react'
+import FeedbackForm from '../Forms/FeedBackForm'
 
 export default function AboutPage() {
   return (
-    <div>AboutPage</div>
+    <div>AboutPage
+    <FeedbackForm /></div>
   )
 }
