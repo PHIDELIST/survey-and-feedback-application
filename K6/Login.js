884eb624f62f66279ecce360d22d3751ded6c29a
PHIDELIST <phidelisoluoch@gmail.com>
2023-09-11 20:57:24 +0300

    wip

--- K6/Login.js
+++ K6/Login.js
@@ -3,12 +3,12 @@ import { check,sleep } from 'k6';
 export const options = {
   stages: [
     {duration: '5s', target: 20  },
-    {duration: '50s', target: 10 },
-    {duration: '20s', target: 0  },
+    {duration: '10s', target: 10 },
+    {duration: '5s', target: 0  },
   ]
 };
 export default function () {
-  const url = 'http://54.167.161.145:8081/auth/login'
+  const url = 'http://localhost:8081/auth/login'
  const payload = JSON.stringify({
     email: 'phidel@gmail.com',
     password: 'Phidel#7',
